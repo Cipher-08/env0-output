ca70b896a64b93a927318656cd41ef00183f2489
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-03-26 16:51:06 +0530

    Update s3_bucket.tf

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,10 +1,28 @@
-resource "aws_s3_bucket" "env0_s3_bucket" {
-  bucket = "my-unique-bucket-name-${random_pet.name.id}"
-  acl    = "private"
+provider "aws" {
+  region = "us-west-2"
 }
 
-resource "random_pet" "name" {}
+resource "aws_instance" "env0-instance-10" {
+  ami           = "ami-0c55b159cbfafe1f0" 
+  instance_type = "t2.micro"
+}
 
-output "bucket_name" {
-  value = aws_s3_bucket.env0_s3_bucket.bucket
+output "instance_id" {
+  value = aws_instance.env0-instance-10.id
+}
+
+output "instance_public_ip" {
+  value = aws_instance.env0-instance-10.public_ip
+}
+
+output "instance_private_ip" {
+  value = aws_instance.env0-instance-10.private_ip
+}
+
+output "instance_availability_zone" {
+  value = aws_instance.env0-instance-10.availability_zone
+}
+
+output "instance_subnet_id" {
+  value = aws_instance.env0-instance-10.subnet_id
 }
