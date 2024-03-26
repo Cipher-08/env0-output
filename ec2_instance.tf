23dfdd88108ebc370f9717a63456eda4c0065e91
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-03-26 15:31:27 +0530

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,13 +1,1 @@
-variable "s3_bucket_name" {
-  type = string
-}
 
-resource "aws_instance" "env0-ex-instance" {
-  ami           = "ami-080e1f13689e07408"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "Env0Instance"
-    "S3 Bucket" = var.s3_bucket_name
-  }
-}
