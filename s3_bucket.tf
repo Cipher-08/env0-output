378d72e0ad904f387aea26a7d8f69a5e1f593768
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-03-26 15:12:45 +0530

    Update s3_bucket.tf

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
-provider "aws" {
-  region     = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
 
 resource "aws_s3_bucket" "env0-s3-bucket-98493838" {
   bucket = "my-unique-bucket-name-${random_pet.name.id}"
