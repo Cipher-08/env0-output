6e20713906d92a4242e8065e02df657ae8c5f6b5
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-03-26 16:44:39 +0530

    Update s3_bucket.tf

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,26 +1,10 @@
-
-
-resource "aws_instance" "env0-bucket-gi-097843" {
-  ami           = "ami-080e1f13689e07408" 
-  instance_type = "t2.micro"
+resource "aws_s3_bucket" "env0_s3_bucket" {
+  bucket = "my-unique-bucket-name-${random_pet.name.id}"
+  acl    = "private"
 }
 
-output "instance_id" {
-  value = aws_instance.env0-bucket-gi-097843.id
-}
+resource "random_pet" "name" {}
 
-output "instance_public_ip" {
-  value = aws_instance.env0-bucket-gi-097843.public_ip
-}
-
-output "instance_private_ip" {
-  value = aws_instance.env0-bucket-gi-097843.private_ip
-}
-
-output "instance_availability_zone" {
-  value = aws_instance.env0-bucket-gi-097843.availability_zone
-}
-
-output "instance_subnet_id" {
-  value = aws_instance.env0-bucket-gi-097843.subnet_id
+output "bucket_name" {
+  value = aws_s3_bucket.env0_s3_bucket.bucket
 }
