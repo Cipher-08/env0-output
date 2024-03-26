bde007da72cc372eb4ba16de6a8d34b6c39f23c2
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-03-26 17:03:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,6 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAZQ3DQ4GW7RLIXIYK"
-  secret_key = "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"
+  access_key = "AKIAZQ3DQ4GW5HNGBY43"
+  secret_key = "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"
 }
